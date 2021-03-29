a3903c2823bf94790bf6eb4b9dca3806aef5ec83
jharris711 <js.harris711@gmail.com>
2021-03-29 15:06:38 -0400

    Step 3

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,47 +1,42 @@
 import React, { useEffect, useRef } from "react";
 import L from "leaflet";
-import { useLeafletContext } from "@react-leaflet/core";
+import { useLeafletContext, createElementHook } from "@react-leaflet/core";
 
-// First, we extract the function that returns bounds from props, 
-// as this logic will be needed in two places:
-const getBounds = props => {
+function getBounds(props) {
   return L.latLng(props.center).toBounds(props.size);
-};
+}
+
+// First, instead of having the Leaflet element creation and updating
+// logic in useEffect callbacks, we can extract them to standalone functions
+// implementing the expected interface:
+function createSquare(props, context) {
+  return { instance: new L.Rectangle(getBounds(props), context) };
+}
+
+function updateSquare(instancem, props, prevProps) {
+  if (props.center !== prevProps.center || props.size !== prevProps.size) {
+    instance.setBounds(getBounds(props));
+  }
+}
+
+// Based on these functions, we can create a useSquareElement hook:
+const useSquareElement = createElementHook(createSquare, updateSquare);
 
 const Square = props => {
   const context = useLeafletContext();
-  // We also need to keep references to the Leaflet element instance 
-  // and the props, by leveraging the useRef hook:
-  const squareRef = useRef();
-  const propsRef = useRef(props);
+  const elementRef = useSquareElement(props, context);
 
-  // Set logic:
-  // Finally, we separate the logic for adding and removing the layer 
-  // from the logic to update it, by setting the dependencies argument 
-  // of the useEffect hook. The first useEffect callback will be only 
-  // called when the component is mounted and unmounted (setting the 
-  // dependencies to []): 
+  // This hook will keep track of the element's instance and props,
+  // so a single useEffect hook can be used to handle the addition and
+  // removal of the layer:
   useEffect(() => {
-    squareRef.current = new L.Rectangle(getBounds(props));
     const container = context.layerContainer || context.map;
-    container.addLayer(squareRef.current);
+    container.addLayer(elementRef.current.instance);
 
     return () => {
-      container.removeLayer(squareRef.current);
+      container.removeLayer(elementRef.current.instance);
     };
   }, []);
-
-  // Update Logic:
-  // The second useEffect callback will be called whenever the props change, and conditionally apply the update to the layer:
-  useEffect(() => {
-    if (
-      props.center !== propsRef.current.center ||
-      props.size !== propsRef.current.size
-    ) {
-      squareRef.current.setBounds(getBounds(props));
-    }
-    propsRef.current = props;
-  }, [props.center, props.size]);
 
   return null;
 };
