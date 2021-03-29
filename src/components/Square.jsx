265ea8edfb9da7c0b93d3f520f350287e5672405
jharris711 <js.harris711@gmail.com>
2021-03-29 14:53:05 -0400

    1 file modified

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,34 +1,48 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import L from "leaflet";
 import { useLeafletContext } from "@react-leaflet/core";
 
+// First, we extract the function that returns bounds from props, 
+// as this logic will be needed in two places:
+const getBounds = props => {
+  return L.latLng(props.center).toBounds(props.size);
+};
+
 const Square = props => {
-  // First, we need to access the context created by the MapContainer
-  // component, by calling the useLeafletContext hook exported by the
-  // core APIs:
   const context = useLeafletContext();
+  // We also need to keep references to the Leaflet element instance 
+  // and the props, by leveraging the useRef hook:
+  const squareRef = useRef();
+  const propsRef = useRef(props);
 
-  // Then, we use React's useEffect hook to create the square instance,
-  // using the props to calculate the bounds to provide to Leaflet's
-  // Rectangle constructor:
+  // Set logic:
+  // Finally, we separate the logic for adding and removing the layer 
+  // from the logic to update it, by setting the dependencies argument 
+  // of the useEffect hook. The first useEffect callback will be only 
+  // called when the component is mounted and unmounted (setting the 
+  // dependencies to []): 
   useEffect(() => {
-    const bounds = L.latLng(props.center).toBounds(props.size);
-    const square = new L.Rectangle(bounds);
-    // The created layer needs to be added to a container provided in the
-    // context, either a parent container such as a LayerGroup, or the Map
-    // instance created with the context:
+    squareRef.current = new L.Rectangle(getBounds(props));
     const container = context.layerContainer || context.map;
-    container.addLayer(square);
+    container.addLayer(squareRef.current);
 
-    // We also need to return the cleaning up function for the useEffect hook,
-    // that removes the layer from the container:
     return () => {
-      container.removeLayer(square);
+      container.removeLayer(squareRef.current);
     };
-  });
+  }, []);
 
-  // Finally, the Square component needs to return a valid React node, but as
-  // the rendering of the layer is performed by Leaflet, it only returns null
+  // Update Logic:
+  // The second useEffect callback will be called whenever the props change, and conditionally apply the update to the layer:
+  useEffect(() => {
+    if (
+      props.center !== propsRef.current.center ||
+      props.size !== propsRef.current.size
+    ) {
+      squareRef.current.setBounds(getBounds(props));
+    }
+    propsRef.current = props;
+  }, [props.center, props.size]);
+
   return null;
 };
 
